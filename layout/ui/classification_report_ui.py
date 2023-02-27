ce3ad83f9e9f6c4cd510292422cad6ce3cbaaa1e
agbleze <lagbleze@st.ug.edu.gh>
2023-02-27 13:16:52 +0100

    Update classification_report_ui.py

--- layout/ui/classification_report_ui.py
+++ layout/ui/classification_report_ui.py
@@ -1,14 +1,47 @@
 from dash import html, dcc
+import dash_bootstrap_components as dbc
 from PIL import Image
 from helper_components import create_img_container
+from arguments import args
 
 
 ## grouped bar graph of various model report
 
-classification_report_layout = html.Div(children=[create_img_container(img_src='',
-                                                                       label='Classification Metrics of various algorithms',
-                                                                       image_style=None
-                                                                       )
+classification_report_layout = html.Div(children=[dbc.Row(children=[create_img_container(img_src=args.img_decision_tree_report,
+                                                                                        label='Classification report on test set for Decision tree',
+                                                                                        image_style=None
+                                                                                        ),
+                                                                    create_img_container(img_src=args.img_extra_decison_report, 
+                                                                                         label='Classification report on test set for Extra Decision tree',
+                                                                                         image_style=None
+                                                                                         ),
+                                                                    create_img_container(img_src=args.img_logistic_report,
+                                                                                         label='Classification report on test set for Logistic Regression',
+                                                                                         image_style=None
+                                                                                         )
+                                                                    ]
+                                                        ),
+                                                  dbc.Row(children=[create_img_container(img_src=args.img_randomforest_report,
+                                                                                        label='Classification report on test set for Random Forest',
+                                                                                        image_style=None
+                                                                                        ),
+                                                                    create_img_container(img_src=args.img_svclinear_report, 
+                                                                                         label='Classification report on test set for Support Vector Classifier (Linear)',
+                                                                                         image_style=None
+                                                                                         ),
+                                                                    create_img_container(img_src=args.img_svcpoly_report,
+                                                                                         label='Classification report on test set for Support Vector Classifier (Polynomial)',
+                                                                                         image_style=None
+                                                                                         )
+                                                                    ]
+                                                        ),
+                                                  dbc.Row(children=[create_img_container(img_src=args.img_svcrbf_report,
+                                                                                         label='Classification report on test set for Support Vector Classifier (Radial)',
+                                                                                         image_style=None
+                                                                                         )
+                                                                    ]
+                                                          )
+                                                  
                                                   ]
                                         )
 
