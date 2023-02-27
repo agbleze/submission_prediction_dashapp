81cb5ec0e04ec5fdd3b1ca5be486473365febb9a
agbleze <lagbleze@st.ug.edu.gh>
2023-02-27 13:17:01 +0100

    Update crossval_ui.py

--- layout/ui/crossval_ui.py
+++ layout/ui/crossval_ui.py
@@ -1,14 +1,24 @@
 from dash import html, dcc
 import dash_bootstrap_components as dbc
-from PIL import Image
 
+from arguments import args
 from helper_components import create_img_container
 
 
 crossval_layout = html.Div(
-                            children=[create_img_container(img_src='', label='Cross Validation Accuracy', 
-                                                           img_style=None
-                                                           )
+                            children=[dbc.Row(children=[create_img_container(img_src=args.img_acc_score, 
+                                                                             label='Cross Validation Accuracy Score', 
+                                                                             image_style=None
+                                                                            ),
+                                                        create_img_container(img_src=args.img_fittime,
+                                                                             label='Fit time for the various models'
+                                                                             ),
+                                                        create_img_container(img_src=args.img_scoretime,
+                                                                             label='Score time for the various models',
+                                                                             image_style=None
+                                                                             )
+                                                        ]
+                                              )
                                     ]
                         )
 
